b977dccc006128bc61b52754a91c193d3be44f19
TalkLess <talklesswan@gmail.com>
2025-08-28 04:14:17 +0000

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
-import Form from "./componennts/Form";
+import Form from "./components/Form";
+import ExpenseList from "./components/ExpenseList";
+import { useState } from "react";
+import ExpenseFilter from "./components/ExpenseFilter";
 
 function App() {
+  const [selectedCategory, setSelecetedCategory] = useState("");
+  const [expenses, SetExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
   return (
     <div>
-      <Form />
+      <div className="mb-5">
+        <Form
+          onSubmit={(expense) =>
+            SetExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelecetedCategory(category)}
+        />
+      </div>
+
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => SetExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </div>
   );
 }
